c12de21d50760d01cee823d65bf54ac452763c9f
Emil <amilsayf@gmail.com>
2021-03-14 17:09:16 +0200

    init

---
+++ META-INF/com/example/contract-rest-service/SNAPSHOT/contracts/find_person_by_id.groovy
@@ -0,0 +1,22 @@
+package com.example.contract-rest-service.SNAPSHOT.contracts
+
+org.springframework.cloud.contract.spec.Contract.make {
+    description "should return person by id=1"
+
+    request {
+        url "/person/1"
+        method GET()
+    }
+
+    response {
+        status OK()
+        headers {
+            contentType applicationJson()
+        }
+        body (
+                id: 1,
+                name: "foo",
+                surname: "bee"
+        )
+    }
+}
